3b0a59d7d135ec179f0659c78e25c490d257e802
fominyh_vv <fominihvv@gamil.com>
2025-02-28 14:15:03 +0300

    some fix, add unitest,

--- choocha/settings.py
+++ choocha/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 # Разрешенные хосты
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(', ')
